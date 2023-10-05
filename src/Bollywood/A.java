52e93c2bb3c180edf193b99728f7633032dc4530
DhruvShirsale007 <workshirsale@gmail.com>
2023-10-05 15:53:20 +0530

    message-my first commit

---
+++ src/Bollywood/A.java
@@ -0,0 +1,13 @@
+package Bollywood;
+
+public class A {
+
+	public static void main(String[] args) {
+		{
+			System.out.println("welcome to bollywood");
+
+		}
+
+	}
+
+}
---
+++ src/Bollywood/B.java
@@ -0,0 +1,12 @@
+package Bollywood;
+
+public class B {
+
+	public static void main(String[] args) {
+		{
+			System.out.println("welcome to hollywood");
+		}
+
+	}
+
+}
